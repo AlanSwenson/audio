a10e2ce73866a1087f36a605f24d8285d886eb72
AlanSwenson <alan@alanswenson.dev>
2021-08-02 16:20:30 -0500

    create shell web app with BS

---
+++ audio/__init__.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request, send_from_directory
+
+
+def create_app():
+    app = Flask(__name__, static_url_path="/static")
+    # app.config.from_object(config_class)
+
+    @app.route("/", methods=["GET"])
+    def root():
+        return render_template(
+            "layout.html",
+            title="Sample Title",
+        )
+
+    # serve files for robots
+    # @app.route("/sitemap.xml")
+    @app.route("/robots.txt")
+    def static_from_root():
+        return send_from_directory(app.static_folder, request.path[1:])
+
+    return app

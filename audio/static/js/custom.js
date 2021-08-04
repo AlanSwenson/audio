7bf87a64fdbcd5af8852abae6b44f31fc50bdc51
AlanSwenson <alan@alanswenson.dev>
2021-08-04 17:43:29 -0500

    customize tippy

---
+++ audio/static/js/custom.js
@@ -0,0 +1,25 @@
+// With the above scripts loaded, you can call `tippy()` with a CSS
+// selector and a `content` prop:
+tippy('#infinite', {
+    content: 'take me to the infinite',
+    inertia: true,
+    animation: 'scale',
+    theme: 'synthwave',
+});
+
+tippy('#listen', {
+    content: 'listen to synthesizers',
+    inertia: true,
+    animation: 'scale',
+    theme: 'synthwave',
+});
+
+tippy('#watch', {
+    content: 'watch cables being patched',
+    inertia: true,
+    animation: 'scale',
+    theme: 'synthwave',
+});
+
+
+

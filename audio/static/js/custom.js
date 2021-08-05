f83a91185254436aabcfe0c164516caed1305d73
AlanSwenson <alan@alanswenson.dev>
2021-08-05 08:22:36 -0500

    refactor tippy

--- audio/static/js/custom.js
+++ audio/static/js/custom.js
@@ -1,25 +1,8 @@
-// With the above scripts loaded, you can call `tippy()` with a CSS
-// selector and a `content` prop:
-tippy('#infinite', {
-    content: 'take me to the infinite',
+// Tippy for Tool tips https://atomiks.github.io/tippyjs/
+tippy('.nav-icons-container a', {
     inertia: true,
     animation: 'scale',
     theme: 'synthwave',
 });
-
-tippy('#listen', {
-    content: 'listen to synthesizers',
-    inertia: true,
-    animation: 'scale',
-    theme: 'synthwave',
-});
-
-tippy('#watch', {
-    content: 'watch cables being patched',
-    inertia: true,
-    animation: 'scale',
-    theme: 'synthwave',
-});
-
 
 
